8613f117a969d6d4c41a8dd6aa4783bed33d4581
aviralpanwar-130 <aviralpanwar263@gmail.com>
2023-05-04 22:20:20 +0530

    CSS

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const path = require('path');
+
 
 const app = express();
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const rootdir = require('./util/path');
 
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use((req,res,next)=>{
+    res.status(404).sendFile(path.join(rootdir,'views','404.html'));
+})
 
 
 app.listen(3000);
--- routes/admin.js
+++ routes/admin.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const path = require('path');
+
+const rootdir = require('../util/path');
 
 const router = express.Router();
 
 router.use(bodyParser.urlencoded({extended:false}));
 
 router.use('/add-product',(req,res,next)=>{
-    res.send(`<form action="/product" method="POST"><input type="text" name="title"><button>Submit</button></form>`);
+    res.sendFile(path.join(rootdir,'views','addProduct.html'));
 });
 
 router.post('/product',(req,res,next)=>{
